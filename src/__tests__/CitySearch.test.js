b38769f20a36ac0aaff0060b494ed22c74e26339
Arni Rohandi <arni@rohandi.com>
2024-12-24 12:58:19 +0100

    Add mock data to test CitySearch
    Signed-off-by: Arni Rohandi <arni@rohandi.com>

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {render} from '@testing-library/react';
 import CitySearch from '../components/CitySearch';
 import userEvent from '@testing-library/user-event';
+import {extractLocations, getEvents} from "../api.js";
 
-describe('<CitySearch /> component', () => {  let CitySearchComponent;
+describe('<CitySearch /> component', () => {
+  let CitySearchComponent;
   beforeEach(() => {
-    CitySearchComponent = render(<CitySearch />);
+    CitySearchComponent = render(<CitySearch/>);
   });
 
   test('renders text input', () => {
@@ -26,5 +28,28 @@ describe('<CitySearch /> component', () => {  let CitySearchComponent;
     const suggestionList = CitySearchComponent.queryByRole('list');
     expect(suggestionList).toBeInTheDocument();
     expect(suggestionList).toHaveClass('suggestions');
+  });
+
+  test('updates list of suggestions correctly when user types in city textbox', async () => {
+    const user = userEvent.setup();
+    const allEvents = await getEvents();
+    const allLocations = extractLocations(allEvents);
+    CitySearchComponent.rerender(<CitySearch allLocations={allLocations}/>);
+
+    // user types "Berlin" in city textbox
+    const cityTextBox = CitySearchComponent.queryByRole('textbox');
+    await user.type(cityTextBox, "Berlin");
+
+    // filter allLocations to locations matching "Berlin"
+    const suggestions = allLocations ? allLocations.filter((location) => {
+      return location.toUpperCase().indexOf(cityTextBox.value.toUpperCase()) > -1;
+    }) : [];
+
+    // get all <li> elements inside the suggestion list
+    const suggestionListItems = CitySearchComponent.queryAllByRole('listitem');
+    expect(suggestionListItems).toHaveLength(suggestions.length + 1);
+    for (let i = 0; i < suggestions.length; i += 1) {
+      expect(suggestionListItems[i].textContent).toBe(suggestions[i]);
+    }
   });
 });
