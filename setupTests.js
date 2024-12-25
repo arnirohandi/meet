07aab2d05ee1ba8bcdcadfae3c484b83bf475932
Arni Rohandi <arni@rohandi.com>
2024-12-25 11:02:13 +0100

    Add couple of integration tests
    Signed-off-by: Arni Rohandi <arni@rohandi.com>

--- setupTests.js
+++ setupTests.js
@@ -1,1 +1,15 @@
 import '@testing-library/jest-dom';
+
+// Here, add portions of the warning messages you want to intentionally prevent from appearing
+const MESSAGES_TO_IGNORE = [
+  "An update to App inside a test was not wrapped in act(...).",
+  "When testing, code that causes React state updates should be wrapped into act(...):",
+  "The current testing environment is not configured to support act(...)"
+];
+
+const originalError = console.error.bind(console.error);
+
+console.error = (...args) => {
+  const ignoreMessage = MESSAGES_TO_IGNORE.find(message => args.toString().includes(message));
+  if (!ignoreMessage) originalError(...args);
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
 import React from 'react';
+import {useEffect, useState, act} from 'react';
 import './App.css'
 import EventList from "./components/EventList.jsx";
 import CitySearch from "./components/CitySearch.jsx";
 import NumberOfEvents from "./components/NumberOfEvents.jsx";
 import mockData from './mock-data';
+import {extractLocations, getEvents} from "./api.js";
 
 const App = () => {
+  const [events, setEvents] = useState([]);
+  const [currentNOE, setCurrentNOE] = useState(32);
+  const [allLocations, setAllLocations] = useState([]);
+  const [currentCity, setCurrentCity] = useState("See all cities");
+
+  useEffect(() => {
+    fetchData();
+  }, [currentCity]);
+
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    const filteredEvents = currentCity === "See all cities" ?
+      allEvents :
+      allEvents.filter(event => event.location === currentCity)
+    setEvents(filteredEvents.slice(0, currentNOE));
+    setAllLocations(extractLocations(allEvents));
+  }
+
   return (
     <div className="App">
-      <CitySearch/>
+      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
       <NumberOfEvents/>
       {/*Test only*/}
       {/*<EventList events={mockData}/>*/}
-      <EventList/>
+      <EventList events={events}/>
     </div>
   );
 }
