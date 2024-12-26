11d710b3f0246ad8a14c78ef8d7e15037f0d55a6
Arni Rohandi <arni@rohandi.com>
2024-12-26 11:09:18 +0100

    Implement atatus APM
    Signed-off-by: Arni Rohandi <arni@rohandi.com>

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import * as atatus from 'atatus-spa';
+
+atatus.config('f472dc3e682e4f4b9569c86b7fa22c81').install();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
